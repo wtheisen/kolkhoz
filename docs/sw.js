9aac119c3b3fafa70abdc73baab74424ea139194
Bill <wtheisen@nd.edu>
2026-07-06 12:30:05 -0400

    Add Kolkhoz marketing site

---
+++ docs/sw.js
@@ -0,0 +1,23 @@
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.keys().then((names) => Promise.all(names.map((name) => caches.delete(name))))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((names) => Promise.all(names.map((name) => caches.delete(name))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(fetch(event.request));
+});
